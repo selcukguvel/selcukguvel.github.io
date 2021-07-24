17c0fcfea0cf0fb25ecfb97c44e2aec6accc5756
selcukguvel <selcukguvel@gmail.com>
2021-07-24 17:45:16 +0300

    Update initial version

--- src/common/UnderDevelopment.js
+++ src/common/UnderDevelopment.js
@@ -1,14 +1,29 @@
-import React from 'react'
-import HourglassFullIcon from '@material-ui/icons/HourglassFull'
-import Grid from '@material-ui/core/Grid'
+import React from "react";
+import HourglassFullIcon from "@material-ui/icons/HourglassFull";
+import Grid from "@material-ui/core/Grid";
 
 export default function UnderDevelopment(props) {
-    return (
-        <Grid item xs={12}>
-            <Grid container alignItems="center" justifyContent="center">
-                <div style={{ paddingRight: 8 }}>Under development</div>
-                <HourglassFullIcon />
-            </Grid>
-        </Grid>
-    )
+  return (
+    <Grid item xs={12}>
+      <Grid
+        container
+        alignItems="center"
+        justifyContent="center"
+        style={{
+          color: "white",
+          paddingTop: 20,
+        }}
+      >
+        <div
+          style={{
+            paddingRight: 8,
+            fontSize: 24,
+          }}
+        >
+          Under development
+        </div>
+        <HourglassFullIcon />
+      </Grid>
+    </Grid>
+  );
 }
--- src/pages/articles/ArticlesPage.js
+++ src/pages/articles/ArticlesPage.js
@@ -1,40 +1,28 @@
-import { makeStyles } from '@material-ui/core/styles'
-import Grid from '@material-ui/core/Grid'
-import UnderDevelopment from '../../common/UnderDevelopment'
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import UnderDevelopment from "../../common/UnderDevelopment";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-    },
-}))
+  root: {
+    flexGrow: 1,
+  },
+}));
 
 export default function ArticlesPage() {
-    const classes = useStyles()
+  const classes = useStyles();
 
-    return (
-        <div className={classes.root}>
-            <Grid
-                container
-                className={classes.root}
-                direction="column"
-                justifyContent="center"
-                alignItems="center"
-                cols={1}
-            >
-                <div
-                    style={{
-                        height: 'auto',
-                        display: 'flex',
-                        justifyContent: 'center',
-                        color: 'white',
-                        paddingTop: 20,
-                        fontSize: 24,
-                    }}
-                >
-                    <UnderDevelopment />
-                </div>
-                {/* <Article/> */}
-            </Grid>
-        </div>
-    )
+  return (
+    <div className={classes.root}>
+      <Grid
+        container
+        className={classes.root}
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        cols={1}
+      >
+        <UnderDevelopment />
+      </Grid>
+    </div>
+  );
 }
